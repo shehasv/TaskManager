27872f21f6b0f03fce9c05b07ee9debb1cfb2b64
Mohamed Shehas <shehasktpm@gmail.com>
2020-11-20 22:19:15 +0530

    first commit

---
+++ src/app/pages/task-view/task-view.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { TaskService } from 'src/app/task.service';
+
+@Component({
+  selector: 'app-task-view',
+  templateUrl: './task-view.component.html',
+  styleUrls: ['./task-view.component.scss']
+})
+export class TaskViewComponent implements OnInit {
+
+  lists:any[];
+  tasks:any[];
+
+  constructor(private taskService:TaskService,private route:ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(
+      (params:Params) =>{
+        this.taskService.getTasks(params.listId).subscribe((tasks:any)=>{
+          this.tasks = tasks;
+        })
+      }
+    )
+    this.taskService.getList().subscribe((lists:any) =>{
+      this.lists = lists;
+    })
+  }
+
+}
---
+++ src/app/task.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { WebRequestService } from './web-request.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor(private webRequestService: WebRequestService) { }
+
+  createList(title:string){
+    return this.webRequestService.post('lists',{title})
+  }
+  createTask(title:string,listId:string){
+    return this.webRequestService.post(`lists/${listId}/tasks`,{title})
+  }
+
+ getList(){
+    return this.webRequestService.get('lists');
+  }
+  getTasks(listId:string){
+    return this.webRequestService.get(`lists/${listId}/tasks`);
+  }
+}
